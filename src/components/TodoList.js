a8d8af68ff57fd778a8db908049ae4af2492247c
surajkumar8274 <76639328+surajkumar8274@users.noreply.github.com>
2022-11-28 19:16:25 +0530

    Add files via upload

---
+++ src/components/TodoList.js
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from 'react';
+import CreateTask from '../modals/CreateTask'
+import Card from './Card';
+import weather from '../Images/weather.png'
+import mood from '../Images/OIP.jpeg'
+import '../components/card.css'
+import Date from "../components/Date"
+
+const TodoList = () => {
+    const [modal, setModal] = useState(false);
+    const [taskList, setTaskList] = useState([])
+    
+    useEffect(() => {
+        let arr = localStorage.getItem("taskList")
+       
+        if(arr){
+            let obj = JSON.parse(arr)
+            setTaskList(obj)
+        }
+    }, [])
+    
+
+    const deleteTask = (index) => {
+        let tempList = taskList
+        tempList.splice(index, 1)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const updateListArray = (obj, index) => {
+        let tempList = taskList
+        tempList[index] = obj
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+    
+
+
+    const saveTask = (taskObj) => {
+        let tempList = taskList
+        tempList.push(taskObj)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(taskList)
+        setModal(false)
+    }
+
+
+    return (
+        <>
+            <div className = "header text-center">
+                <h3>Todo List</h3>
+                <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)} >Create Task</button>
+                <Date />
+                <img className='weather' src ={mood} alt =" "/>
+                    <img className='weather' src ={weather} alt =" "/>
+                    <p className='weather'>20" F - cold</p>
+            </div>
+            <div className = "task-container">
+            {taskList && taskList.map((obj , index) => <Card taskObj = {obj} index = {index} deleteTask = {deleteTask} updateListArray = {updateListArray}/> )}
+            </div>
+            <CreateTask toggle = {toggle} modal = {modal} save = {saveTask}/>
+        </>
+    );
+};
+
+export default TodoList;
